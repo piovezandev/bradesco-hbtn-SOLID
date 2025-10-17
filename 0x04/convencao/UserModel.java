5ebaf3942183b0482aae358fd19865c55fabdb70
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-10-16 21:48:10 -0300

    bradesco-hbtn-SOLID - convencao

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,25 @@
+// classe users, representando nossa entidade
+public class UserModels {
+
+    private int userId;
+
+    private String descricao;
+
+    private final static int IDADE_MINIMA = 18;
+
+    // função que coleta todos os users
+    public User[] fetchUsers() {
+        return new User[0];
+    }
+
+    // função que coleta todos os users
+    public User[] getAllUsers() {
+        return new User[0];
+    }
+
+    // função que coleta todos os users
+    public User[] getUsers() {
+        return new User[0];
+    }
+
+}
